3fef49d3109344816f27b1db83e15027a6c434cb
Amitabh Sharma <amitabh.sharma39@gmail.com>
2018-12-04 18:30:37 +0530

    added route for player

--- src/app/module/video/video-routing.module.ts
+++ src/app/module/video/video-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { VideoLandingMainComponent } from './video-landing-main/video-landing-main.component';
+import { VideoPlayerMainComponent } from './video-player-main/video-player-main.component';
 
 const routes: Routes = [
-  { path: '', component: VideoLandingMainComponent}
+  { path: '', component: VideoLandingMainComponent},
+  { path: 'player', component: VideoPlayerMainComponent}
 ];
 
 @NgModule({
