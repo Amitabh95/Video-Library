56992c58e235160d19b6e685678cd646fbc79fa1
Amitabh Sharma <amitabh.sharma39@gmail.com>
2018-12-04 18:29:54 +0530

    added content in video landing and player page

--- src/app/module/video/video-landing-main/video-landing-main.component.ts
+++ src/app/module/video/video-landing-main/video-landing-main.component.ts
@@ -17,6 +17,7 @@ export class VideoLandingMainComponent implements OnInit {
     private router: Router) { }
 
   ngOnInit() {
+    this.getAllVideos();
   }
 
   signOut() {
@@ -35,9 +36,87 @@ export class VideoLandingMainComponent implements OnInit {
   getAllVideos() {
     this.firestoreDB.getAllVideoPlaylist().then((response: any) => {
       this.allVideoPlaylist = response.response;
-      console.log('Response---> ', response);
+      this.allVideoPlaylist = this.sortSeries();
     }).catch((error) => {
       console.log('Error---> ', error);
     });
+  }
+
+  timeDifference(timestamp) {
+    let finalTime: string;
+    let difference = Date.now() - timestamp;
+
+    const month = Math.floor(difference / 1000 / 60 / 60 / 24 / 30);
+    difference -= month * 1000 * 60 * 60 * 24 * 30;
+
+    const days = Math.floor(difference / 1000 / 60 / 60 / 24);
+    difference -= days * 1000 * 60 * 60 * 24;
+
+    const hours = Math.floor(difference / 1000 / 60 / 60);
+    difference -= hours * 1000 * 60 * 60;
+
+    const minutes = Math.floor(difference / 1000 / 60);
+    difference -= minutes * 1000 * 60;
+
+    const seconds = Math.floor(difference / 1000);
+
+    if (month === 0 && days === 0 && hours === 0) {
+      if (minutes === 0) {
+        finalTime = 1 + ' minute';
+      } else {
+        finalTime = minutes + ' minutes';
+      }
+    } else if (month === 0 && days === 0 && hours > 0) {
+      if (hours === 1) {
+        finalTime = hours + ' hour';
+      } else {
+        finalTime = hours + ' hours';
+      }
+    } else if (month === 0 && days > 0) {
+      if (days === 1) {
+        finalTime = days + ' day';
+      } else {
+        finalTime = days + ' days';
+      }
+    } else if (month > 0) {
+      if (month === 1) {
+        finalTime = month + ' month';
+      } else {
+        finalTime = month + ' months';
+      }
+    }
+    return finalTime;
+  }
+
+  calculateEpisodes(dataArray) {
+    let final = dataArray.length;
+    if (final === 0) {
+      final = 0;
+    } else if (final === 1) {
+      final = 1 + ' episode';
+    } else if (final > 1) {
+      final = final + ' episodes';
+    }
+    return final;
+  }
+
+  sortSeries() {
+    let tempArray = JSON.parse(JSON.stringify(this.allVideoPlaylist));
+    return tempArray.sort(this.compare);
+  }
+
+  compare(a, b) {
+    if (a.sortOrder > b.sortOrder) {
+      return -1;
+    }
+    if (a.sortOrder < b.sortOrder) {
+      return 1;
+    }
+    return 0;
+  }
+
+  playVideo(data) {
+    console.log('Play video--> ', data);
+    this.router.navigate(['/player'], { queryParams: { videoID: data.videoID } });
   }
 }
--- src/app/module/video/video-player-main/video-player-main.component.ts
+++ src/app/module/video/video-player-main/video-player-main.component.ts
@@ -1,15 +1,189 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FirestoreDatabaseService } from 'src/app/shared/services/firebase/firestoreDatabase/firestore-database.service';
+
 
 @Component({
   selector: 'app-video-player-main',
   templateUrl: './video-player-main.component.html',
   styleUrls: ['./video-player-main.component.css']
 })
-export class VideoPlayerMainComponent implements OnInit {
+export class VideoPlayerMainComponent implements OnInit, OnDestroy {
+  videoID: string;
+  uid: string;
+  videoPlaylist: any;
+  routeSubscription: any;
+  youtubePlayer: any;
+  videoLiked: boolean;
+  videoLikes: number;
+  seriesID: string;
+  isEpisode: boolean;
+  episodeID: number;
+  episodeVideoData: any;
+  videoObject: any;
+  YT: any;
+  player: any;
+  reframed: Boolean = false;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private firestoreDB: FirestoreDatabaseService
+  ) {
+    this.isEpisode = false;
+  }
 
   ngOnInit() {
+    this.uid = localStorage.getItem('uid');
+    this.routeSubscription = this.route
+      .queryParams
+      .subscribe(params => {
+        this.videoID = params['videoID'] || 0;
+        const containDashAt = this.videoID.indexOf('-');
+        // if ( containDashAt === -1) {
+        this.getPlaylist(this.videoID);
+        // } else {
+        //   this.getEpisodeVideo(this.videoID);
+        //   console.log('dash at---> ', containDashAt);
+        // }
+      });
+  }
+
+  getEpisodeVideo(videoID) {
+    const containDashAt = videoID.indexOf('-');
+    const videoid = videoID.substring(0, containDashAt);
+    console.log('videoid-->', Number(videoid));
+    console.log('episode--> ', Number(videoID.substring(containDashAt + 1)));
+  }
+
+  getPlaylist(videoID) {
+    const containDashAt = videoID.indexOf('-');
+    if (containDashAt === -1) {
+      this.isEpisode = false;
+      this.firestoreDB.getVideoPlaylist(videoID).then((result: any) => {
+        if (!result.error) {
+          this.videoPlaylist = result.response;
+          this.videoLikes = this.videoPlaylist.likes.likes;
+          this.videoLiked = false;
+          this.videoObject = this.videoPlaylist.URL;
+          this.videoPlaylist.likes.likedBy.forEach(element => {
+            if (element === this.uid) {
+              this.videoLiked = true;
+              return false;
+            }
+          });
+          this.initiatePlayer();
+        }
+      }).catch((error) => {
+        console.log(error);
+      });
+    } else {
+      this.isEpisode = true;
+      this.seriesID = videoID.substring(0, containDashAt);
+      this.episodeID = Number(videoID.substring(containDashAt + 1));
+      this.firestoreDB.getVideoPlaylist(this.seriesID).then((result: any) => {
+        if (!result.error) {
+          this.videoPlaylist = result.response;
+          this.episodeVideoData = result.response.episode[this.episodeID];
+          this.videoLikes = this.episodeVideoData.likes.likes;
+          this.videoObject = this.episodeVideoData.URL;
+          this.videoLiked = false;
+          this.episodeVideoData.likes.likedBy.forEach(element => {
+            if (element === this.uid) {
+              this.videoLiked = true;
+              return false;
+            }
+          });
+          this.initiatePlayer();
+        }
+      });
+    }
+  }
+
+  getYoutubeVideoID(videoURL) {
+    const videoid = videoURL.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+    if (videoid != null) {
+      console.log('video id = ', videoid[1]);
+      return videoid[1];
+    }
+  }
+
+  initiatePlayer() {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    window['onYouTubeIframeAPIReady'] = (e) => {
+      this.YT = window['YT'];
+      this.reframed = false;
+      this.player = new window['YT'].Player('video-container', {
+        videoId: this.getYoutubeVideoID(this.videoObject.video),
+        playerVars: {
+          showinfo: 0,
+          modestbranding: 1,
+          start: localStorage.getItem('pause-time')
+        },
+        events: {
+          'onStateChange': this.onPlayerStateChange.bind(this),
+          'onReady': (e) => {
+            if (!this.reframed) {
+              this.reframed = true;
+            }
+          }
+        }
+      });
+    };
+  }
+  onPlayerStateChange(event) {
+    console.log('Hello..', event);
+    switch (event.data) {
+      case window['YT'].PlayerState.PLAYING:
+        if (this.cleanTime() === 0) {
+          console.log('started ' + this.cleanTime());
+        } else {
+          console.log('playing ' + this.cleanTime());
+        }
+        break;
+      case window['YT'].PlayerState.PAUSED:
+        if (this.player.getDuration() - this.player.getCurrentTime() !== 0) {
+          console.log('paused' + ' @ ' + this.cleanTime());
+          localStorage.setItem('pause-time', this.cleanTime().toString());
+        }
+        break;
+      case window['YT'].PlayerState.ENDED:
+        console.log('ended ');
+        break;
+    }
+  }
+
+  cleanTime() {
+    return Math.round(this.player.getCurrentTime());
+  }
+
+  likeIncreament() {
+    if (!this.isEpisode) {
+      console.log('like series');
+      this.videoPlaylist.likes.likedBy.forEach(element => {
+        if (element !== this.uid) {
+          this.firestoreDB.incrementSeriesVideoLike(this.videoPlaylist, null, this.uid);
+        }
+      });
+    } else {
+      console.log('like episode');
+      this.episodeVideoData.likes.likedBy.forEach(element => {
+        if (element !== this.uid) {
+          this.firestoreDB.incrementSeriesVideoLike(this.videoPlaylist, this.episodeID, this.uid);
+          console.log('videoData--> ', this.videoPlaylist);
+          console.log('episodeID--> ', this.episodeID);
+          console.log('uid--> ', this.uid);
+        }
+      });
+    }
+  }
+
+  ngOnDestroy() {
+
   }
 
 }
